5cc454e63afa2cd325831b0420a97934bcd98b77
hb5588973 <152051429+hb5588973@users.noreply.github.com>
2024-04-04 04:53:51 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -20,7 +20,7 @@ let urls = [
 	// 添加更多订阅,支持base64
 ];
 
-let subconverter = "apiurl.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
+let subconverter = "psub.wsbon.workers.dev"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
 let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_Full.ini"; //订阅配置文件
 
 export default {
